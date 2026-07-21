6b51c9b50525cf34bcf5c48ac334b0b5bcbc1862
Thiago Pessanha <tiagofelipe15@gmail.com>
2026-07-21 18:59:00 +0000

    exclusão dos BAT para retirar os arquivos do windows

--- gestao_contratos_sdap_web/ATUALIZAR_SISTEMA_WINDOWS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call .venv\Scripts\activate.bat
-python -m pip install -r requirements.txt
-python manage.py migrate --noinput
-python manage.py bootstrap_system --no-admin
-python manage.py collectstatic --noinput
-python manage.py check
-pause
